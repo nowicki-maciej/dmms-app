567749b62a7987e3df5d1db6d1c5b0634fd19219
Michał Krajewski <Krajewski.M@protonmail.com>
2019-03-23 17:47:08 +0100

    Logout improvements.

--- user-management/src/main/java/cf/dmms/app/usermanagement/SecurityConfig.java
+++ user-management/src/main/java/cf/dmms/app/usermanagement/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
@@ -20,51 +21,57 @@ import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private UserPrincipalService userPrincipalService;
-    private UnauthorizedHandler unauthorizedHandler;
+	private UserPrincipalService userPrincipalService;
+	private UnauthorizedHandler unauthorizedHandler;
 
-    public SecurityConfig(UserPrincipalService userPrincipalService, UnauthorizedHandler unauthorizedHandler) {
-        this.userPrincipalService = userPrincipalService;
-        this.unauthorizedHandler = unauthorizedHandler;
-    }
+	public SecurityConfig(UserPrincipalService userPrincipalService, UnauthorizedHandler unauthorizedHandler) {
+		this.userPrincipalService = userPrincipalService;
+		this.unauthorizedHandler = unauthorizedHandler;
+	}
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .userDetailsService(userPrincipalService)
-                .passwordEncoder(passwordEncoder());
-    }
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+				.userDetailsService(userPrincipalService)
+				.passwordEncoder(passwordEncoder());
+	}
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf()
-                    .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                    .and()
-                .logout()
-                    .clearAuthentication(true)
-                    .logoutUrl("/user-management/logout")
-                    .and()
-                .exceptionHandling()
-                    .authenticationEntryPoint(unauthorizedHandler)
-                    .and()
-                .authorizeRequests()
-                    .antMatchers("/user-management/login", "/user-test-management/createUser")
-                        .permitAll()
-                    .anyRequest()
-                        .authenticated();
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+				.csrf()
+					.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+					.and()
+				.logout()
+					.invalidateHttpSession(true)
+					.deleteCookies("JSESSIONID")
+					.logoutUrl("/user-management/logout")
+					.logoutSuccessHandler(logoutSuccessHandler())
+					.and()
+				.exceptionHandling()
+					.authenticationEntryPoint(unauthorizedHandler)
+					.and()
+				.authorizeRequests()
+					.antMatchers("/user-management/login", "/user-test-management/createUser")
+						.permitAll()
+					.anyRequest()
+						.authenticated();
+	}
 
-    }
+	//changing default spring security behaviour
+	private LogoutSuccessHandler logoutSuccessHandler() {
+		return (request, response, authentication) -> response.setStatus(200);
+	}
 
-    @Bean(BeanIds.AUTHENTICATION_MANAGER)
-    @Override
-    public AuthenticationManager authenticationManager() throws Exception{
-        return super.authenticationManager();
-    }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+	@Bean(BeanIds.AUTHENTICATION_MANAGER)
+	@Override
+	public AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
+	}
 
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
